2bae37b455d0d9d4a4d954e3f39f60e3f89568ac
ranieriprg <eddieranieri.prg@gmail.com>
2024-07-04 00:50:17 -0300

    criacao do server + rota de usuario, controler e post com retorno json no postman

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+//config JSON response
+app.use(express.json());
+
+//solve cors
+app.use(cors({ credentials: true, origin: "http://locahost:3000" }));
+
+//public folder for images
+app.use(express.static("public"));
+
+//routes
+const UserRoutes = require("./routes/UserRoutes");
+app.use("/users", UserRoutes);
+
+app.listen(5000);
